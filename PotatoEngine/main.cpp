a632d769fab0b154826d6ad094b6b94d47e6aedf
suola-makkara <haavisto.roope@gmail.com>
2021-12-20 17:09:08 +0200

    window creation

--- PotatoEngine/main.cpp
+++ PotatoEngine/main.cpp
@@ -1,14 +1,46 @@
-#include "GLFW\glfw3.h"
-#include "glm\vec3.hpp"
 #include "glad\glad.h"
+#define GLFW_INCLUDE_NONE
+#include "GLFW\glfw3.h"
+
+//#include "glm\vec3.hpp"
 
 #include <iostream>
 
+void errorCallback(int error, const char* description);
+
 int main()
 {
-	glm::vec3 vec(1, 2, 3);
+	// Initialize GLFW
+	if (!glfwInit())
+		return EXIT_FAILURE;
 
-	std::cout << vec.x << '\n';
+	glfwSetErrorCallback(errorCallback);
 
-	return 0;
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+	GLFWwindow* window = glfwCreateWindow(800, 800, "Test", nullptr, nullptr);
+	if (!window)
+		return EXIT_FAILURE;
+
+	glfwMakeContextCurrent(window);
+	glfwSwapInterval(1);
+
+	gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+
+		glfwSwapBuffers(window);
+	}
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
+
+	return EXIT_SUCCESS;
+}
+
+void errorCallback(int error, const char* description)
+{
+	std::cout << "GLFW: " << description << '\n';
 }
